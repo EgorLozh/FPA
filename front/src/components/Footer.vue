2b6e8897e0495b290655d66ed421b9d4f2d31c6e
Artur Sobolev <dragoogard@mail.ru>
2025-01-12 23:12:16 +0400

    Implement new feature for report generation

---
+++ front/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer">
+        <p>&copy; 2024 ScriptTracker</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60px;
+    background-color: #f8f9fa;
+    border-top: 1px solid #e7e7e7;
+}
+
+.footer p {
+    margin: 0;
+    font-size: 14px;
+    color: #6c757d;
+}
+</style>
